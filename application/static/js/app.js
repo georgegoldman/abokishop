aca4df2590afc0b9ad4147b2e4eb5b3eff5cae14
georgegoldman <georgegoldman2014@gmail.com>
2020-01-06 11:37:53 +0100

    updt

--- application/static/js/app.js
+++ application/static/js/app.js
@@ -1,17 +1,21 @@
 
 x = document.querySelector('#content-box');
 y = document.querySelector('#v-pills-tab');
+z = document.querySelector('#nav-btn-open');
+i = document.querySelector('#nav-btn-close');
 
-var navToggle = function() {
-    if(x.style.display == 'block' && y.style.display == 'none'){
-        console.log('connected!');
-        x.style.display = "none";
-        y.style.display = "block";
-    }else{
-        console.log('not connected!')
-        x.style.display = "block";
-        y.style.display = "none";
-    }
+var showNav = function() {
+    x.style.display = 'none';
+    y.style.display = 'block';
+    z.style.display = 'none';
+    i.style.display = 'block'
+}
+
+var hideNav = function() {
+    x.style.display = 'block';
+    y.style.display = 'none';
+    z.style.display = 'block';
+    i.style.display = 'none';
 }
 
 // var resetNavToggle = function() {
@@ -19,6 +23,6 @@ var navToggle = function() {
 //     y.style.display = "none";
 // }
 
-document.querySelector('#nav-btn').addEventListener('click', navToggle);
+// document.querySelector('#nav-btn').addEventListener('click', navToggle);
 // document.getElementsByTagName('body').addEventListener('click', resetNavToggle);
 
