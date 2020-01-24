3da95f3b4ea82544ab94f372e6a43eaaae3673f8
georgegoldman <georgegoldman2014@gmail.com>
2020-01-24 20:52:36 +0100

    updt

--- application/__init__.py
+++ application/__init__.py
@@ -8,8 +8,8 @@ from flask_fontawesome import FontAwesome
 app = Flask(__name__)
 
 app.config['DEBUG']=1
-app.config['SECRET_KEY']=os.environ.genviron.get('SECRET')
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.genviron.get('DATABASE_URL')
+app.config['SECRET_KEY']=os.environ.get('SECRET')
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 # app.config['SECRET_KEY'] = 'negjrhi52452325'
 # app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:password@localhost/abokishop'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
